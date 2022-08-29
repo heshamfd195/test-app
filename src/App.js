fd36f76de0c46f82b6ed3b6fa2df1d7b71935e11
Hesham <hesham.fouad195@gmail.com>
2022-08-29 19:12:22 +0500

    first commi2w2t

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           Testing the app on netlify 1.2v
+           Testing the app on netlify 1.5v
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-           Hex 3D App
+           Hex 3D Configurator
         </a>
       </header>
     </div>
