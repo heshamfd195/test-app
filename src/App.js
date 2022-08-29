2933f9c4b12df095d4307d2d67cf6fffd7094f6f
Hesham <hesham.fouad195@gmail.com>
2022-08-29 19:06:51 +0500

    first commi2w2t

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+           Testing the app on netlify 1.2v
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+           Hex 3D App
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
